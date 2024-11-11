35ef8ddddf2d085e50304170c163a7f1578a9795
BienNguyen2005 <minhhieuanlao1970@gmail.com>
2024-11-11 21:57:24 +0700

    not done

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ import css from './assets/css/main.css'
 import scr from './assets/js/main.js'
 import  './assets/vendor/bootstrap/css/bootstrap.css'
 import js from './assets/vendor/bootstrap/js/bootstrap.bundle'
-const app = createApp(App).use(bootstrap, css, scr, img, js)
+import Master from './views/pages/Master.vue'
+const app = createApp(App).use(bootstrap, css, scr, img, js, Master)
 
 app.use(router)
 
